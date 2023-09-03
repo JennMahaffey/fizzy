4ebd518cd6764419c83309c91234fea928f41cb3
jennifer mahaffey <jmahaffey1.prsvr@gmail.com>
2023-09-03 12:11:16 -0400

    start over

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
  * YOUR CODE GOES RIGHT HERE! *
  ******************************/
 
-for (var i = 1; i < 101; i++) {
+function() i = 1; i < 101; i++) {
   if (i % 15 == 0) console.log("FizzBuzz");
   else if (i % 3 == 0) console.log("Fizz");
   else if (i % 5 ==0) console.log("Buzz");
